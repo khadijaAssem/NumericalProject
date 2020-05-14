cfd71e9fead4f5ebc6ddc84a60aa52e972751172
norhanmagdi <58911005+norhanmagdi@users.noreply.github.com>
2020-05-14 04:45:36 +0200

    Add files via upload

--- Open_methods/NewtonRaphson.py
+++ Open_methods/NewtonRaphson.py
@@ -1,21 +1,23 @@
 import sympy
 import numpy
+import math
 import timeit
+import matplotlib.pyplot as plt
 
 
 class NR:
     def __init__(self, fn, xi, max_iterations, epsilon):
         self.iterations = []
-        self.ea = float(0)
-        self.xr = float(0)
+        self.ea = 0
+        self.xr = 0
         self.number_of_iterations = 0
-        self.xi = float(xi)
+        self.xi = xi
         self.fn = fn
-        if max_iterations == 0:
+        if max_iterations is None:
             self.max_iterations = 50
         else:
             self.max_iterations = max_iterations
-        if epsilon == 0:
+        if epsilon is None:
             self.epsilon = 0.0001
         else:
             self.epsilon = epsilon
@@ -57,14 +59,43 @@ class NR:
 
         execution_time = timeit.default_timer() - start_time
 
-        return self.number_of_iterations, execution_time, self.iterations, self.xr, self.ea
+        return self.number_of_iterations, execution_time, self.iterations, self.xr, self.ea, value_eqn
+
+    def draw_plot(iterations, function):
+        iteration = iterations[0]
+        xi = iteration['current_approximate']
+        xr = iteration['approximate_root']
+        diff = max(xi, xr) - min(xi, xr)
+        start = min(xi, xr) - 0.25 * 10*diff
+        end = max(xi, xr) + 0.25 * 10*diff
+        if diff == 0:
+            start = -10
+            end = 10
+        xpts = numpy.linspace(start, end, 100)
+        OX = 0 * xpts
+        # x = np.linspace(-1,2)             # define the mesh in (-1,2)
+        # OX = 0*x
+
+        plt.plot(xpts, function(xpts), label='f(x)', color='b')
+        plt.axhline(y=0, color='k')
+        plt.axvline(x=0, color='k')
+        plt.plot([xi, xi], [0, function(xi)], color='y', label='Xi')
+        plt.plot([xr, xr], [0, function(xr)], color='r', label='Xi+1')
+        plt.legend()
+        plt.show()
+
 
 x = sympy.Symbol('x')
-obj = NR((x ** 3 - 7 * x ** 2 + 8 * x - 3), 5, 0 ,0)
+obj = NR(0.95*x**3-5.9*x**2+10.9*x-6, 3.5, None, None)
+number_of_iterations, execution_time, iterations, xr, ea, value_eqn = obj.solve()
+#Result = obj.solve()
+#print(Result[0])
+#print(Result[1])
+#print(*Result[2], sep=", ")
+#print(Result[3])
+#print(Result[4])
+#print(Result[5])
 
-Result = obj.solve()
-print(Result[0])
-print(Result[1])
-print(*Result[2], sep=", ")
-print(Result[3])
-print(Result[4])
+#0.95*x**3-5.9*x**2+10.9*x-6, 3.5
+
+NR.draw_plot(iterations, value_eqn)
--- Open_methods/Secant.py
+++ Open_methods/Secant.py
@@ -1,6 +1,8 @@
 import sympy
 import numpy
 import timeit
+import matplotlib.pyplot as plt
+
 
 
 class SC:
@@ -59,4 +61,43 @@ class SC:
 
         execution_time = timeit.default_timer() - start_time
 
-        return self.number_of_iterations, execution_time, self.iterations, self.xr, self.ea
+        return self.number_of_iterations, execution_time, self.iterations, self.xr, self.ea, value_eqn
+
+    def draw_plot(iterations, function):
+        iteration = iterations[0]
+        xi = iteration['current_approximate']
+        xr = iteration['approximate_root']
+        diff = max(xi, xr) - min(xi, xr)
+        start = min(xi, xr) - 0.25 * 10 * diff
+        end = max(xi, xr) + 0.25 * 10 * diff
+        if diff == 0:
+            start = -10
+            end = 10
+        xpts = numpy.linspace(start, end, 100)
+        OX = 0 * xpts
+        # x = np.linspace(-1,2)             # define the mesh in (-1,2)
+        # OX = 0*x
+
+        plt.plot(xpts, function(xpts), label='f(x)', color='b')
+        plt.axhline(y=0, color='k')
+        plt.axvline(x=0, color='k')
+        plt.plot([xi, xi], [0, function(xi)], color='y', label='Xi')
+        plt.plot([xr, xr], [0, function(xr)], color='r', label='Xi+1')
+        plt.legend()
+        plt.show()
+
+
+x = sympy.Symbol('x')
+obj = SC(x ** 2 - 2, 0.5, 1, 0, 0)
+number_of_iterations, execution_time, iterations, xr, ea, value_eqn = obj.solve()
+# Result = obj.solve()
+# print(Result[0])
+# print(Result[1])
+# print(*Result[2], sep=", ")
+# print(Result[3])
+# print(Result[4])
+# print(Result[5])
+
+# 0.95*x**3-5.9*x**2+10.9*x-6, 3.5
+
+SC.draw_plot(iterations, value_eqn)

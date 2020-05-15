63ef5cc7a63e06745c493e7d878579040bc75d7a
norhanmagdi <58911005+norhanmagdi@users.noreply.github.com>
2020-05-15 22:28:39 +0200

    Add files via upload

---
+++ Open_methods/fixedTRY.py
@@ -0,0 +1,44 @@
+import timeit
+import numpy
+import sympy
+from sympy.abc import x
+import matplotlib.pyplot as plt
+
+
+def solve(equation, approx_root, max_iterations=50, epsilon=0.0001):
+    number_iterations = 0
+    start_time = timeit.default_timer()
+    try:
+        simple_fun = sympy.simplify(equation)
+        print(simple_fun)
+        symbol = simple_fun.free_symbols.pop()
+        print(symbol)
+
+        simple_fun = sympy.Add(symbol, simple_fun)
+        print(simple_fun)
+        function = sympy.lambdify(symbol, simple_fun)
+        print(function(2))
+    except ValueError:
+        raise ValueError("Not a valid function")
+
+    prev_app=approx_root
+    error = 100
+    iterations = []
+    while True:
+        approx_root = function(prev_app)
+        error = abs((approx_root - prev_app) / approx_root) * 100
+        iteration = numpy.array((prev_app, approx_root, error),
+                                dtype=[('prev_app', numpy.float), ('approx_root', numpy.float),
+                                       ('err', numpy.float)])
+        iterations.append(iteration)
+        prev_app=approx_root
+        number_iterations += 1
+        if error < epsilon or number_iterations > max_iterations:
+            break
+
+    execution_time = timeit.default_timer() - start_time
+
+
+    return number_iterations, execution_time, iterations, approx_root, error, function
+number_iterations, execution_time, iterations, approx_root, error, function = solve(x**2 - 2*x - 3, 0, 50, 0.0001)
+print(approx_root)
